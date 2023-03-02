ea56a708d423dfaaa220e513572827ad74a6bd1a
Daveamegs <amegah40@gmail.com>
2023-03-02 22:14:40 +0000

    create about placeholder

--- src/about/About.js
+++ src/about/About.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
+import profileImage from "../images/leilani-angel-unsplash.jpg";
 
 const about = () => {
   return (
-    <div>
-      about
+    <div className="about">
+      <div className="about-left">
+        <div className="about-left-prof-img">
+          <img src={profileImage} alt="Profile" />
+        </div>
+        <div className="about-profile-desc"></div>
+      </div>
+      <div className="about-right">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          fuga, assumenda soluta earum facilis expedita quia exercitationem sed
+          iure maxime. Asperiores consequuntur officia sit, quam quas blanditiis
+          itaque laudantium ea? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Sapiente fuga, assumenda soluta earum facilis
+          expedita quia exercitationem sed iure maxime. Asperiores consequuntur
+          officia sit, quam quas blanditiis itaque laudantium ea?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          fuga, assumenda soluta earum facilis expedita quia exercitationem sed
+          iure maxime. Asperiores consequuntur officia sit, quam quas blanditiis
+          itaque laudantium ea? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Sapiente fuga, assumenda soluta earum facilis
+          expedita quia exercitationem sed iure maxime. Asperiores consequuntur
+          officia sit, quam quas blanditiis itaque laudantium ea?
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
